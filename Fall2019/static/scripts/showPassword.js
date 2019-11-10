743a7eccbec49d429fac653a7732c422355c82d5
tripletm26 <46800783+tripletm26@users.noreply.github.com>
2019-11-09 22:49:53 -0500

    fa fa eye for showPassword

--- Fall2019/static/scripts/showPassword.js
+++ Fall2019/static/scripts/showPassword.js
@@ -1,13 +1,15 @@
 function viewPassword()
 {
   var passwordInput = document.getElementById('tbPassword');
-  //var passStatus = document.getElementById('btnShowPass');
+  var passStatus = document.getElementById('pass-status');
  
   if (passwordInput.type == 'password'){
-    passwordInput.type='text';    
+    passwordInput.type='text';
+    passStatus.className='fa fa-eye-slash';    
   }
   else{
     passwordInput.type='password';
+    passStatus.className='fa fa-eye';
   }
 }
 
@@ -15,14 +17,20 @@ function viewPassword1()
 {
   var passwordInput = document.getElementById('tbPass');
   var passwordInput2 = document.getElementById('tbConfirm');
-  //var passStatus = document.getElementById('btnShowPass');
+  var passStatus = document.getElementById('pass-status');
+  var passStatus1 = document.getElementById('pass-status1');
  
   if (passwordInput.type == 'password' || passwordInput1.type == 'password'){
     passwordInput.type='text';
     passwordInput2.type='text';   
+    passStatus.className='fa fa-eye-slash';
+    passStatus1.className='fa fa-eye-slash';
   }
+
   else{
     passwordInput.type='password';
     passwordInput2.type='password';
+    passStatus.className='fa fa-eye';
+    passStatus1.className='fa fa-eye';
   }
 }
