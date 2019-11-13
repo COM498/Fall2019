8d16831725360a27933cb055f08fc82aa0f57bfe
Brianna Weaver <bnweaver98@gmail.com>
2019-11-13 10:26:11 -0500

    image downloading
    image downloading

--- WebApp/static/scripts/dashboard.js
+++ WebApp/static/scripts/dashboard.js
@@ -243,12 +243,19 @@ $(document).ready(function() {
                     attach.textContent = result.recordset[i]["filename"];
                     attach.setAttribute("href", "/files/" + result.recordset[i]["filepath"]);
                     //attach.value = result.recordset[i]["filepath"];
-                    // attach.onclick = function(event) {
-                    //     var href = "/files/" + document.getElementById(event.target.id).value;
-                    //     //window.open("file:///" + href);
-
-                    //     window.open(href);
-                    // }
+                    // attach.setAttribute("href", "javascript:;");
+                    attach.onclick = function(event) {
+                        // $.ajax({
+                        //     type: "POST",
+                        //     url: "/api/getfiles",
+                        //     data: '{"FileName": "' + document.getElementById(event.target.id).value + '"}',
+                        //     contentType: "application/json"
+                        // }).done(function(result) {
+                        //     alert(result);
+                        //     console.log(result);
+                        // });
+                        alert("File downloaded");
+                    }
                 }
 
                 var hint = document.createElement("p");
--- WebServer/server.js
+++ WebServer/server.js
@@ -1,15 +1,20 @@
 const http = require('http');
 const fs = require('fs');
+const fssync = require('fs-sync');
 const path = require('path');
+const downloads = require('downloads-folder');
 
 const port = 80;
-//var FILE_PATH = "C:\\Users\\brianna.weaver\\Documents\\GitHub\\Fall2019\\WebApp\\Build Notes\\"; 
+//var FILE_PATH = "C:\\Users\\brianna.weaver\\Documents\\"; 
 var FILE_PATH = process.env.FILE_PATH;
+var DOWNLOAD_PATH = downloads();
 
 var app = http.createServer(function(req, res) {
     console.log("Requesting file...");
 
     var filePath = FILE_PATH + req.url.split('/')[1];
+    //var downPath = DOWNLOAD_PATH + "\\" + req.url.split('/')[1];
+    var downPath = req.url.split('/')[1];
     console.log(req.url);
     console.log(filePath);
 
@@ -28,24 +33,41 @@ var app = http.createServer(function(req, res) {
         '.txt': 'text/plain'
       };
 
-    contentType = mimeTypes[extname];
+      contentType = mimeTypes[extname];
 
-    if (extName =='.jpg' || extName == '.png' || extName == '.ico' || extName == '.eot' || extName == '.ttf' || extName == '.svg') {
-        let file = fs.readFileSync(filePath);
-        res.setHeader("'Content-Type'", "'" + contentType + "'");
-        res.write(file, 'binary');
-        res.end();
+    if (extname =='.jpg' || extname == '.png' || extname == '.ico' || extname == '.eot' || extname == '.ttf' || extname == '.svg') {
+        // let file = fs.readFileSync(filePath);
+        // res.setHeader("'Content-Type'", "'" + contentType + "'");
+        // res.write(file, 'binary');
+        // res.end();
+
+        fssync.copy(filePath, downPath, {encoding: 'binary'}, function(err) {
+            if (err) console.error(err.stack);
+            else {
+                console.log(downPath);
+                res.end("OK");
+            }
+        })
     } 
     else {
-        fs.readFile(filePath, 'utf8', function(err, content) {
-            if (err) {
-                console.log("Error: " + err.code);
-            }
+
+        fs.copyFile(filePath, downPath, function(err) {
+            if (err) { console.error(err.stack); }
             else {
-                res.setHeader("'Content-Type'", "'" + contentType + "'");
-                res.end(content);
+                console.log(downPath);
+                res.end("OK");
             }
-        });
+        })
+
+        // fs.readFile(filePath, 'utf8', function(err, content) {
+        //     if (err) {
+        //         console.log("Error: " + err.code);
+        //     }
+        //     else {
+        //         res.setHeader("'Content-Type'", "'" + contentType + "'");
+        //         res.end(content);
+        //     }
+        // });
         
     }
 });
