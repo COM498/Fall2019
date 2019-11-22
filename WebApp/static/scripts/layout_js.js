5b162e42cdc3532cea447d47d4f0db2cc024ce2e
tripletm26 <46800783+tripletm26@users.noreply.github.com>
2019-11-22 10:52:09 -0500

    font changes

--- WebApp/static/scripts/layout_js.js
+++ WebApp/static/scripts/layout_js.js
@@ -1,26 +1,13 @@
-var $win = $(window);
-var $lay = $('#layout');
-var baseSize = {
-    w: 720,
-    h: 500    
-}
-
-function updateScale() {
-    
-    var ww = $win.width();
-    var wh = $win.height();
-    var newScale = 1;
-    
-    // compare ratios
-    if(ww/wh < baseSize.w/baseSize.h) { // tall ratio
-        newScale = ww / baseSize.w;
-    } else { // wide ratio
-        newScale = wh / baseSize.h;        
-    }
-    
-    $lay.css('transform', 'scale(' + newScale + ',' +  newScale + ')');
-    
-    console.log(newScale);
-}
-
-$(window).resize(updateScale);
+jQuery(window).resize(function()  
+{  
+    var ratio = 3/4; // height / width  
+    jQuery('#resizeMe').height( jQuery('#resizeMe').width() * ratio );  
+});  
+  
+// When the page loads, trigger a window resize event  
+// so our element gets resized by default. Saves having   
+// to duplicate the same code on load too.  
+jQuery(window).load(function()  
+{  
+    jQuery(window).trigger('resize');  
+}); 
